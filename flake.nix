886c577b513cf07b2663fa68411e269e4217853c
Jarrod Pas <jarrod@jarrodpas.com>
2021-03-28 01:15:57 -0600

    adds preliminary nix flake support

---
+++ flake.nix
@@ -0,0 +1,45 @@
+{
+  description = "A collection of packages related to homomorphic encryption";
+
+  inputs = {
+    nixpkgs = {
+      url = "github:nixos/nixpkgs/nixos-unstable";
+    };
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [ "x86_64-linux" ];
+
+      forAllSystems = f: nixpkgs.lib.genAttrs supportedSystems (system: f system);
+
+      pkgsFor = system: import nixpkgs {
+        inherit system;
+        overlays = self.overlays;
+      };
+    in
+    {
+      overlay = final: prev:
+        let
+          he = {
+            #heaan = final.callPackage ./pkgs/heaan { };
+            helib = final.callPackage ./pkgs/helib { };
+            palisade = final.callPackage ./pkgs/palisade { };
+            seal = final.callPackage ./pkgs/seal { };
+            tfhe = final.callPackage ./pkgs/tfhe { };
+            tfhepp = final.callPackage ./pkgs/tfhepp { };
+          };
+        in
+        he // { inherit he; };
+
+      overlays = [ self.overlay ];
+
+      packages = forAllSystems (system: (pkgsFor system).he);
+
+      defaultPackage = forAllSystems (system:
+        let pkgs = pkgsFor system; in
+        pkgs.linkFarmFromDrvs "he-packages-unstable"
+          (builtins.attrValues pkgs.he)
+      );
+    };
+}
